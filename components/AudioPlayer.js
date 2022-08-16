0ef496fc96827dd80189a3214d901d4af0ba27a3
Theal Altake <zufa.altake@gmail.com>
2022-08-16 04:40:13 +0300

    css style

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/AudioPlayer.module.css";
+import { BsArrowLeftShort } from "react-icons/bs";
+import { BsArrowRightShort } from "react-icons/bs";
+import { FaPlay } from "react-icons/fa";
+import { FaPause } from "react-icons/fa";
 
 const AudioPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlayPause = () => {
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <div className={styles.audioPlayer}>
       <audio
         src="https://media.blubrry.com/muslim_central_quran/podcasts.qurancentral.com/saud-al-shuraim/saud-al-shuraim-002.mp3"
         preload="metadata"
       ></audio>
-      <button>back 30</button>
-      <button>play / pause </button>
-      <button>forward 30</button>
-
+      <button className={styles.forwardBackward}>
+        <BsArrowLeftShort /> 30
+      </button>
+      <button onClick={togglePlayPause} className={styles.playPause}>
+        {isPlaying ? <FaPause /> : <FaPlay className={styles.play} />}
+      </button>
+      <button className={styles.forwardBackward}>
+        <BsArrowRightShort />
+        30
+      </button>
       {/* Current time */}
-      <div>0:00</div>
-
+      <div className={styles.currentTime}>0:00</div>
       {/* Progress bar */}
       <input type="range" />
-
       {/* Duration time */}
-      <div>2:49</div>
+      <div className={styles.duration}>2:49</div>
     </div>
   );
 };
